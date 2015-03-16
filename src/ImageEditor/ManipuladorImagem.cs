98bb4662a20b215050fc8b466aa921cb08bc4359
fhelwanger <fhelwanger@gmail.com>
2015-03-15 22:42:15 -0300

    Commit inicial - abrir imagem + escala de cinza

---
+++ src/ImageEditor/ManipuladorImagem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEditor
+{
+    public class ManipuladorImagem
+    {
+        private Bitmap bitmap;
+
+        public Image Imagem
+        {
+            get
+            {
+                return bitmap;
+            }
+        }
+
+        public void CarregarImagem(string caminho)
+        {
+            bitmap = new Bitmap(Image.FromFile(caminho));
+        }
+
+        public void TransformarEscalaCinza()
+        {
+            AbrirBytesImagem(bytes =>
+            {
+                for (int i = 0; i < bytes.Length; i += 3)
+                {
+                    var cinza = (byte)(bytes[i] * 0.3 + bytes[i + 1] * 0.59 + bytes[i + 2] * 0.11);
+                    bytes[i] = cinza;
+                    bytes[i + 1] = cinza;
+                    bytes[i + 2] = cinza;
+                }
+            });
+        }
+
+        private void AbrirBytesImagem(Action<byte[]> acao)
+        {
+            var area = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            var bmpData = bitmap.LockBits(area, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int numBytes = bmpData.Stride * bmpData.Height;
+            var bytes = new byte[numBytes];
+
+            Marshal.Copy(bmpData.Scan0, bytes, 0, numBytes);
+
+            acao(bytes);
+
+            Marshal.Copy(bytes, 0, bmpData.Scan0, numBytes);
+
+            bitmap.UnlockBits(bmpData);
+        }
+    }
+}
---
+++ src/ImageEditor/frmImageEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageEditor
+{
+    public partial class frmImageEditor : Form
+    {
+        public frmImageEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var img1 = new ManipuladorImagem();
+            img1.CarregarImagem(@"D:\Lenna.png");
+            pictureBox1.Image = img1.Imagem;
+
+            var img2 = new ManipuladorImagem();
+            img2.CarregarImagem(@"D:\Lenna.png");
+            img2.TransformarEscalaCinza();
+            pictureBox2.Image = img2.Imagem;
+        }
+    }
+}
